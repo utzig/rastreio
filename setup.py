ab8cd48fd6affd77f16613254ec6c074d8b0ebb3
Fabio Utzig <utzig@utzig.org>
2017-05-04 16:59:51 -0300

    Add dependencies, bump version

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup
 
 setup(
     name = "rastreio",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Fabio Utzig",
     author_email = "utzig@utzig.org",
     description = ("Utility to track packages in the Brazilian post site"),
     license = "MIT",
     url = "http://github.com/utzig/rastreio",
     packages=["rastreio"],
+    install_requires=[
+        'requests',
+        'gevent',
+    ],
     scripts=["rastreio/rastreio.py"],
     entry_points = {
         "console_scripts": [ "rastreio = rastreio:main" ]
